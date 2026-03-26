8245dc3e8f5fbe9dc5b236497bc2845828f1500e
adityaooooo <adittyasarkar9090@gmail.com>
2026-03-26 19:04:36 +0600

    Add files via upload

---
+++ managers/passenger_manager.py
@@ -0,0 +1,80 @@
+
+import json
+from models.passenger import Passenger
+class PassengerManager:
+
+    def __init__(self, file_path):
+        self.file_path = file_path        
+        self.passengers = self.load_passengers()
+
+    def load_passengers(self):
+        passengers = []    
+        try:
+            with open(self.file_path, "r") as f:
+                data = json.load(f)
+                    
+                if not isinstance(data, list):
+                    data = []
+             
+                for p in data:
+                    try:
+                        passengers.append(Passenger.from_dict(p))
+                    except KeyError:
+                        continue
+                   
+                          
+    
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError:
+            pass 
+        
+        return passengers
+                                      
+    def save_passengers(self):
+        with open(self.file_path, "w") as f:
+            json.dump([p.to_dict() for p in self.passengers], f, indent=4)
+    
+    def add_passenger(self, passenger):
+        if self.find_passenger(passenger.passenger_id):
+            print("Error: Passenger with this id already exists!")
+            return False
+        self.passengers.append(passenger)
+        self.save_passengers()
+        return True
+                                                                                          
+    def list_passengers(self):
+        for p in self.passengers:
+            print(p)
+            
+    def find_passenger(self, passenger_id):
+        for p in self.passengers:
+            if p.passenger_id == passenger_id:
+               return p 
+        return None
+                                                                                                                                                                                          
+    def delete_passenger(self, passenger_id):
+        passenger = self.find_passenger(passenger_id)
+        if passenger:
+            self.passengers.remove(passenger)
+            self.save_passengers()
+            return True
+        return False
+    
+    def update_passenger(self, passenger_id, name=None, age=None, passport=None, phone=None):
+        passenger = self.find_passenger(passenger_id)
+        
+        if passenger:
+            if name:
+                passenger.name = name
+            if age:
+                passenger.age = age
+            if passport:
+                passenger.passport = passport
+            if phone:
+                passenger.phone = phone
+            
+            self.save_passengers()
+            return True
+    
+        return False                 
